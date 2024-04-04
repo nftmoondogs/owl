e9ad251fbdd674ca16ccd5c2e55ab5e849af8204
nftmoondogs <nftmoondogs@gmail.com>
2024-04-04 06:03:43 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         </a>
         <div className="flex items-center">
           {/* Social Icons */}
-          <a href="https://twitter.com/woofsoltoken" target="_blank" rel="noopener noreferrer" className="text-white px-3">
+          <a href="https://twitter.com/owltokensol" target="_blank" rel="noopener noreferrer" className="text-white px-3">
           <svg className="fill-current text-white hover:text-gray-400" width="30" height="30" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
               {/* Replace with your Twitter path */}
               <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
